4861b56be5dfb920b640dde8b7ac125d1e586e6c
maticanzic <matic.anzic@gmail.com>
2019-01-10 16:51:27 +0100

    Popravki baze

--- sql/db.sql
+++ sql/db.sql
@@ -76,7 +76,7 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES 
 (1,'Matic', 'Zahradnik', 'matic@ep.si', 'matic1', 0, '', '', '', true),
 (2, 'Vanesa', 'Godec', 'vanesa@ep.si', 'nesi1', 1, '', '', '', true),
-(3, 'Maja', 'Lobnik', 'maja@lobnik.si', 'majci1', 2, 'Večna pot 113', 5, '040123456', '', true);
+(3, 'Maja', 'Lobnik', 'maja@lobnik.si', 'majci1', 2, 'Večna pot 113', 5, '040123456', true);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -105,15 +105,6 @@ CREATE TABLE `order_article` (
   CONSTRAINT `FK_oa_order` FOREIGN KEY(`id_order`) REFERENCES `order`(`id`),
   CONSTRAINT `FK_oa_article` FOREIGN KEY (`id_article`) REFERENCES `article`(`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
 
 DROP TABLE IF EXISTS `post_office`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -132,12 +123,20 @@ CREATE TABLE `post_office` (
 LOCK TABLES `post_office` WRITE;
 /*!40000 ALTER TABLE `post_office` DISABLE KEYS */;
 INSERT INTO `post_office` VALUES 
-(1, `1000 Ljubljana`),
-(2, `2000 Maribor`),
-(3, `3000 Celje`),
-(4, `4000 Kranj`),
-(5, `5000 Nova Gorica`)
+(1, '1000 Ljubljana'),
+(2, '2000 Maribor'),
+(3, '3000 Celje'),
+(4, '4000 Kranj'),
+(5, '5000 Nova Gorica');
 /*!40000 ALTER TABLE `post_office` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 -- Dump completed on 2014-12-12 16:45:04
