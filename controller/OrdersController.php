def2ac03ba06b800bee5c8703c9e96d444245f78
vg0691 <vg0691@student.uni-lj.si>
2019-01-12 00:13:32 +0100

    xss zdej pa res

--- controller/OrdersController.php
+++ controller/OrdersController.php
@@ -1,6 +1,7 @@
 <?php
     header('X-Frame-Options: SAMEORIGIN');
     header('X-Content-Type-Options: nosniff');
+    header("X-XSS-Protection: 1; mode=block");
     require_once("model/OrderDB.php");
     require_once("ViewHelper.php");
 
